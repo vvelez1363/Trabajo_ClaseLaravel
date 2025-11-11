df3c9b8ad041e2cc3c49507687c64e55cfcb4b07
vvelez1363 <valentina.velez34629@ucaldas.edu.co>
2025-11-10 20:52:36 -0500

    Uso del https2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(app()->environment('production')) {
+        if($this->app->environment('production')) {
             URL::forceScheme('https');
         }
     }
 }
+
